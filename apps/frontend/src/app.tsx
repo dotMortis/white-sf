4a13acdd6935e374b04bae9f927622a82b38ee17
Danksa <gdanksa@gmail.com>
2024-09-21 15:33:28 +0200

    Added overlay component

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,4 +1,5 @@
 import './app.css';
+import { Overlay } from './components/overlay/overlay.js';
 import { PlayingTable } from './components/playing-table/playing-table.js';
 import { ScoreDisplay } from './components/score-display/score-display.js';
 import { VoteChart } from './components/vote-chart/vote-chart.js';
@@ -9,6 +10,9 @@ export const App = () => {
             <PlayingTable />
             <ScoreDisplay dealerScore={0} playerScore={0} />
             <VoteChart votes={[4, 6]} categories={['Draw', 'Pass']} />
+            <Overlay open={true}>
+                <p style={{ backgroundColor: 'white' }}></p>
+            </Overlay>
         </main>
     );
 };
