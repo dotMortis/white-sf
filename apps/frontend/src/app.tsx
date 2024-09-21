5fa667a750d3848f7d93c5b389b4ce97963aeeb1
Danksa <gdanksa@gmail.com>
2024-09-21 15:07:50 +0200

    Created simple vote chart component

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,12 +1,14 @@
 import './app.css';
 import { PlayingTable } from './components/playing-table/playing-table.js';
 import { ScoreDisplay } from './components/score-display/score-display.js';
+import { VoteChart } from './components/vote-chart/vote-chart.js';
 
 export const App = () => {
     return (
         <main className='app'>
             <PlayingTable />
             <ScoreDisplay />
+            <VoteChart votes={[4, 6]} categories={['Draw', 'Pass']} />
         </main>
     );
 };
