0c9e85ad37f1c94b6dbe279606394fe9b25d236d
Nick Köllhofer <nick.koellhofer@braun-its.de>
2024-09-22 22:31:40 +0200

    update paths remove some loggin

--- apps/backend/src/routes/telephone.routes.ts
+++ apps/backend/src/routes/telephone.routes.ts
@@ -8,19 +8,12 @@ export function TELEPHONE_ROUTER(theGame: TheGame) {
     const TELEFON_ROUTER = Router({ mergeParams: true });
     const controller = new TelephoneController(theGame);
 
-    LOGGER.debug('Creating router: /register');
     TELEFON_ROUTER.route('/register').get(controller.registerHandler());
-    LOGGER.debug('Creating router: /hang-up');
-    TELEFON_ROUTER.route('/hang-up').get(controller.hangUpHandler());
-    LOGGER.debug('Creating router: /start-game');
-    TELEFON_ROUTER.route('/start-game').get(controller.startGameHandler());
-    LOGGER.debug('Creating router: /vote-pass');
-    TELEFON_ROUTER.route('/vote-pass').get(controller.votePassHandler());
-    LOGGER.debug('Creating router: /vote-draw');
-    TELEFON_ROUTER.route('/vote-draw').get(controller.voteDrawHandler());
-    LOGGER.debug('Creating router: /state');
+    TELEFON_ROUTER.route('/hangup').get(controller.hangUpHandler());
+    TELEFON_ROUTER.route('/start').get(controller.startGameHandler());
+    TELEFON_ROUTER.route('/pass').get(controller.votePassHandler());
+    TELEFON_ROUTER.route('/draw').get(controller.voteDrawHandler());
     TELEFON_ROUTER.route('/state').get(controller.stateHandler());
-    LOGGER.debug('Creating router: /action');
     TELEFON_ROUTER.route('/action').get(controller.actionHandler());
     return TELEFON_ROUTER;
 }
